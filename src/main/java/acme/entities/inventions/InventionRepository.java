e808757efb789dde9a1821cf9b42d3b9609d2ac2
AlejandroParody <alejandroparodyq@gmail.com>
2026-03-02 20:58:57 +0100

    fix: fixed invention query

--- src/main/java/acme/entities/inventions/InventionRepository.java
+++ src/main/java/acme/entities/inventions/InventionRepository.java
@@ -2,7 +2,6 @@
 package acme.entities.inventions;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -10,7 +9,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface InventionRepository extends AbstractRepository {
 
-	@Query("select sum(p.cost.amount) from Part p where p.invention.id = :inventionId")
-	Double computeTotalCost(@Param("id") int inventionId);
+	@Query("select sum(p.cost.amount) from Part p where p.invention.id = :id")
+	Double computeTotalCost(int id);
 
 }
