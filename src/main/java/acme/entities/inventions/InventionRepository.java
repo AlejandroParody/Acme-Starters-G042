5316284e5c2173d2935fe9d7f69676953a59925f
BLuzio <biasini-pedo@insa-toulouse.fr>
2026-03-15 11:55:14 +0100

    fix + up to date with main

--- src/main/java/acme/entities/inventions/InventionRepository.java
+++ src/main/java/acme/entities/inventions/InventionRepository.java
@@ -9,12 +9,7 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface InventionRepository extends AbstractRepository {
 
-<<<<<<< HEAD
-	@Query("select sum(p.cost.amount) from Part p where p.invention.id = :inventionId")
-	Double computeTotalCost(@Param("inventionId") int inventionId);
-=======
 	@Query("select sum(p.cost.amount) from Part p where p.invention.id = :id")
 	Double computeTotalCost(int id);
->>>>>>> refs/remotes/origin/main
 
 }
