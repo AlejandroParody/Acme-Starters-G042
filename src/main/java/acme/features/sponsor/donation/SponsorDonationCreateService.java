6b1f4f42934835b0dd8038948c6a0c14097778d5
Jose <josmorlop10@alum.us.es>
2026-03-18 20:11:11 +0100

    added last features about donation creation/update/delete in sponsorships

---
+++ src/main/java/acme/features/sponsor/donation/SponsorDonationCreateService.java
@@ -0,0 +1,80 @@
+
+package acme.features.sponsor.donation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.models.Tuple;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractService;
+import acme.datatypes.DonationKind;
+import acme.entities.sponsorships.Donation;
+import acme.entities.sponsorships.Sponsorship;
+import acme.realms.Sponsor;
+
+@Service
+public class SponsorDonationCreateService extends AbstractService<Sponsor, Donation> {
+
+	@Autowired
+	private SponsorDonationRepository	repository;
+
+	private Donation					donation;
+
+
+	@Override
+	public void load() {
+		int sponsorshipId;
+		Sponsorship sponsorship;
+
+		sponsorshipId = super.getRequest().getData("sponsorshipId", int.class);
+		sponsorship = this.repository.findSponsorshipById(sponsorshipId);
+
+		this.donation = super.newObject(Donation.class);
+		this.donation.setSponsorship(sponsorship);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int sponsorshipId;
+		Sponsorship sponsorship;
+
+		sponsorshipId = super.getRequest().getData("sponsorshipId", int.class);
+		sponsorship = this.repository.findSponsorshipById(sponsorshipId);
+
+		status = sponsorship != null && sponsorship.getDraftMode() && sponsorship.getSponsor().isPrincipal();
+
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.donation, "name", "notes", "money", "kind");
+	}
+
+	@Override
+	public void validate() {
+		super.validateObject(this.donation);
+	}
+
+	@Override
+	public void execute() {
+		this.repository.save(this.donation);
+	}
+
+	@Override
+	public void unbind() {
+		Tuple tuple;
+		SelectChoices choices;
+
+		choices = SelectChoices.from(DonationKind.class, this.donation.getKind());
+
+		tuple = super.unbindObject(this.donation, "name", "notes", "money", "kind");
+
+		tuple.put("kinds", choices);
+		tuple.put("sponsorshipId", this.donation.getSponsorship().getId());
+		tuple.put("published", !this.donation.getSponsorship().getDraftMode());
+
+		super.getResponse().addData(tuple);
+	}
+}
